887c42e1b8c7588bc02e11abf71f36d3206e8a6b
minsang98 <blood89@nate.com>
2022-10-21 14:16:11 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Count = styled.div``;
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +14,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Count>
+          {count}
+          <div className="event-box">
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+          </div>
+        </Count>
       </header>
     </div>
   );
